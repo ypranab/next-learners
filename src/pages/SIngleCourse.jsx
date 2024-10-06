3ea4bff484e9011354c1c59ff5df7e3376ee1adc
ypranab11 <ypranab11@gmail.com>
2024-10-06 23:54:35 +0600

    details page updated

--- src/pages/SIngleCourse.jsx
+++ src/pages/SIngleCourse.jsx
@@ -5,7 +5,19 @@ const SingleCourse = () => {
   const course = useLoaderData();
   const navigate = useNavigate();
 
-  const { author, title, details, img_url } = course;
+  const {
+    author,
+    title,
+    details,
+    img_url,
+    author_img_url,
+    ratings,
+    lession,
+    level,
+    duration,
+    price,
+    student,
+  } = course;
 
   const handleToastCart = () => {
     toast.success(`${title} is added to cart succesfully!!`);
@@ -16,17 +28,31 @@ const SingleCourse = () => {
   }
 
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl w-3/4 mx-auto m-10 py-16">
-      <figure className="max-w-screen-xl">
+    <div className="card lg:card-side bg-teal-50 shadow-xl w-3/4 mx-auto mb-16 py-4">
+      <figure className="w-svw">
         <img src={`${img_url}`} alt="Album" />
       </figure>
-      <div className="card-body">
+      <div className="card-body font-semibold">
         <h2 className="card-title">{title}</h2>
-        <p>Author: {author}</p>
-        <p>Review: {details}</p>
+        <div className="flex items-center">
+          <div className="avatar">
+            <div className="mask mask-squircle w-16">
+              <img src={author_img_url} />
+            </div>
+          </div>
+          <span>Author: {author}</span>
+        </div>
+
+        <p>Details: {details}</p>
+        <p>Ratings: {ratings}</p>
+        <p>Duration: {duration}</p>
+        <p>Students Enrolled: {student}</p>
+        <p>Level: {level}</p>
+        <p>Lessions: {lession}</p>
+        <p>Price: {price}</p>
 
         <div className="card-actions justify-end">
-          <button onClick={handleToastCart} className="btn btn-primary">
+          <button onClick={handleToastCart} className="btn btn-primary mr-20">
             Add to Cart
           </button>
         </div>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 export const ROUTES = {
   BASE_URL: "https://next-learners-server.vercel.app",
   HOME: "/",
-  COURSES: "/api/products",
+  COURSES: "/api/products/",
   SINGLE_COURSE: {
     STATIC: "/api/products/:id",
     DYNAMIC: (course_id) => `/api/products/${course_id}`,
